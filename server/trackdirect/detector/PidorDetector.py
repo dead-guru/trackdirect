1e92a412e4414b91e01d1d990da331ee97cf7e66
assada <assada.ua@gmail.com>
2023-10-16 23:50:00 +0300

    Russia detecting via direct geojson

--- server/trackdirect/detector/PidorDetector.py
+++ server/trackdirect/detector/PidorDetector.py
@@ -1,15 +1,22 @@
 import logging
 import string
-from geopy.geocoders import Nominatim
+import json
+import os
+from geojson import Point, MultiPolygon, Feature
+from turfpy.measurement import boolean_point_in_polygon
 
 from trackdirect.common.Singleton import Singleton
+
 
 class PidorDetector(Singleton):
     def __init__(self):
         self.logger = logging.getLogger(__name__)
         self.blackList = self._fillBlackList()
-        self.geolocator = Nominatim(user_agent="trackdirect", domain='geoc.dead.guru')
-        self.geoCache = []
+        print ([f for f in os.listdir() if os.path.isfile(f)])
+        file_path = os.path.join(os.path.dirname(__file__), "russia.json")
+        with open(file_path, "r") as f:
+            data = json.load(f)
+            self.polygon = Feature(geometry=MultiPolygon(data['geometry']['coordinates']))
 
     def _fillBlackList(self):
         """Fill block list with stations that should be ignored
@@ -18,38 +25,36 @@ class PidorDetector(Singleton):
         ascii_uppercase = list(string.ascii_uppercase)
         for letter in ascii_uppercase:
             block_list.append("R" + letter)
+            block_list.append("P-R" + letter)
 
         for letter in ascii_uppercase:
             block_list.append("U" + letter)
+            block_list.append("P-U" + letter)
             if letter == "I":
                 break
 
         for num in range(0, 10):
             block_list.append("R" + str(num))
+            block_list.append("P-R" + str(num))
 
         # block_list.append("UU") Crimea
+        block_list.append("MOSCOW")
 
         return block_list
 
     def _detectByCoordinates(self, lat, long):
-        crdString = str(round(lat, 4)) + ", " + str(round(long, 4))
-        if crdString in self.geoCache:
-            return True
-        else:
-            location = self.geolocator.reverse(query=crdString)
-            if location is not None:
-                    if 'address' in location.raw and 'country_code' in location.raw['address'] and location.raw['address']['country_code'] == 'ru':
-                        self.geoCache.append(crdString)
-                        return True
-                    else:
-                        return False
-            return False
+        point = Feature(geometry=Point((round(long, 5), round(lat, 5))))
+        result = boolean_point_in_polygon(point, self.polygon)
+        self.logger.info("Detected (" + str(round(lat, 5)) + ", " + str(round(long, 5)) + ") by coordinates: " + str(result))
+        return result
 
     def isPidor(self, callsign, lat, long):
         if any(callsign.startswith(item) for item in self.blackList):
+            self.logger.info("Detected by callsign: " + str(callsign))
             return True
         else:
-            if lat is not None and long is not None and any(callsign.startswith(item) for item in ["UU", "XU", "UW", "XW", "XS", "ZA", "ZF", "US", "ZE", "UN", "ZC", "UO", "ZB", "UR", "XU", "ZD", "UL", "XR", "XL", "XN", "XH"]):
+            if lat is not None and long is not None and any(callsign.startswith(item) for item in ["UU", "XU", "UW", "XW", "XS", "ZA", "ZF", "US", "ZE", "UN", "ZC", "UO", "ZB", "UR", "XU", "ZD", "UL", "XR", "XL", "XN", "XH", "HD", "HF", "HC"]):
+                self.logger.info("Detecting by coordinates: " + str(callsign))
                 return self._detectByCoordinates(lat, long)
             else:
                 return False
